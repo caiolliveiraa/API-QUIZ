0866c41b142f15b2544c01fd78628d32b758ec62
Caio Oliveira <caiohenrique3335@gmail.com>
2023-06-15 14:19:36 -0300

    Initial commit

---
+++ src/main/java/com/example/apiquiz/ApiQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiQuizApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/apiquiz/Level.java
@@ -0,0 +1,2 @@
+package com.example.apiquiz;public class Level {
+}
